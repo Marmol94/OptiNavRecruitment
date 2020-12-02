bd1accf044aef65ae0fadc0f6e6efde6cf3241a0
Marmol94 <michmarmol94@gmail.com>
2020-12-03 00:49:10 +0100

    ExtensionIdentifier.Identify functionality. Tests passed

--- ImageProcessingLibrary.Test/UnitTest1.cs
+++ ImageProcessingLibrary.Test/UnitTest1.cs
@@ -16,22 +16,28 @@ namespace ImageProcessingLibrary.Test
         {
             var png = File.ReadAllBytes("Szreki/Shrek.png");
             var result = ExtensionIdentifier.Identify(png);
-            Assert.Equals(result, Extensions.Png);
+            Assert.AreEqual(result, Extensions.Png);
         }
         [Test]
         public void IsJpg()
         {
             var jpg = File.ReadAllBytes("Szreki/Shrek.jpg");
             var result = ExtensionIdentifier.Identify(jpg);
-            Assert.Equals(result, Extensions.Jpg);
+            Assert.AreEqual(result, Extensions.Jpg);
         }
         [Test]
         public void IsBmp()
         {
-            
             var bmp = File.ReadAllBytes("Szreki/Shrek.bmp");
             var result = ExtensionIdentifier.Identify(bmp);
-            Assert.Equals(result, Extensions.Bmp);
+            Assert.AreEqual(result, Extensions.Bmp);
+        }
+        [Test]
+        public void IsUnknown()
+        {
+            var unknown = new byte[] {0x00, 0x00, 0x00, 0x00};
+            var result = ExtensionIdentifier.Identify(unknown);
+            Assert.AreEqual(result, Extensions.Unknown);
         }
     }
 }
--- ImageProcessingLibrary/ExtensionIdentifier.cs
+++ ImageProcessingLibrary/ExtensionIdentifier.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 
 namespace ImageProcessingLibrary
 {
@@ -7,7 +8,29 @@ namespace ImageProcessingLibrary
     {
         public static Enum Identify(byte[] image)
         {
-            
+            var pngMagicNumber = new byte[] {0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A};
+            var bmpMagicNumber = new byte[] {0x42, 0x4D};
+            var jpgMagicNumber = new byte[] {0xFF, 0xD8, 0xFF};
+            var isPng = image.Take(8).ToArray().SequenceEqual(pngMagicNumber);
+            var isBmp = image.Take(2).ToArray().SequenceEqual(bmpMagicNumber);
+            var isJpg = image.Take(3).ToArray().SequenceEqual(jpgMagicNumber);
+
+            if (isPng)
+            {
+                return Extensions.Png;
+            }
+
+            if (isBmp)
+            {
+                return Extensions.Bmp;
+            }
+
+            if (isJpg)
+            {
+                return Extensions.Jpg;
+            }
+
+            return Extensions.Unknown;
         }
     }
     public enum Extensions
