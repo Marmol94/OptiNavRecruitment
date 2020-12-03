2da393af50aaa5cd22d526a23f8dda28d686432b
Marmol94 <michmarmol94@gmail.com>
2020-12-03 21:12:01 +0100

    Correct magic number array.

--- ImageProcessingLibrary/ExtensionIdentifier.cs
+++ ImageProcessingLibrary/ExtensionIdentifier.cs
@@ -10,25 +10,19 @@ namespace ImageProcessingLibrary
         {
             var pngMagicNumber = new byte[] {0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A};
             var bmpMagicNumber = new byte[] {0x42, 0x4D};
-            var jpgMagicNumber = new byte[] {0xFF, 0xD8, 0xFF};
+            var jpgMagicNumber = new byte[] {0xFF, 0xD8};
             var isPng = image.Take(8).ToArray().SequenceEqual(pngMagicNumber);
             var isBmp = image.Take(2).ToArray().SequenceEqual(bmpMagicNumber);
-            var isJpg = image.Take(3).ToArray().SequenceEqual(jpgMagicNumber);
+            var isJpg = image.Take(2).ToArray().SequenceEqual(jpgMagicNumber);
 
             if (isPng)
-            {
                 return Extensions.Png;
-            }
 
             if (isBmp)
-            {
                 return Extensions.Bmp;
-            }
 
             if (isJpg)
-            {
                 return Extensions.Jpg;
-            }
 
             return Extensions.Unknown;
         }
