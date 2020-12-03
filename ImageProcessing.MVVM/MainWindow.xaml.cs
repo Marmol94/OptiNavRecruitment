1546961227b86a58aebe974f879c543af53fb02b
Marmol94 <michmarmol94@gmail.com>
2020-12-03 21:12:16 +0100

    Add front-end.

---
+++ ImageProcessing.MVVM/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using Microsoft.Win32;
+
+namespace ImageProcessing.MVVM
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public static readonly DependencyProperty FilePathProperty = DependencyProperty.Register(
+            nameof(FilePath), typeof(string), typeof(MainWindow), new PropertyMetadata(default(string)));
+
+        public string FilePath
+        {
+            get => (string) GetValue(FilePathProperty);
+            set => SetValue(FilePathProperty, value);
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainWindowViewModel();
+        }
+
+
+        private void OnButtonClick(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog {Filter = "Image Files(*.BMP;*.JPG;*.PNG)|*.BMP;*.JPG;*.PNG"};
+            if (dialog.ShowDialog() != true) return;
+
+            FilePathTextBox.Text = dialog.FileName;
+            FilePathTextBox.GetBindingExpression(TextBox.TextProperty)?.UpdateSource();
+        }
+    }
+}
---
+++ ImageProcessing.MVVM/MainWindowViewModel.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using JetBrains.Annotations;
+using Microsoft.Win32;
+
+namespace ImageProcessing.MVVM
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action<object> _execute;
+        private readonly Func<object?, bool> _canExecute;
+    
+        public RelayCommand(Action<object> execute, Func<object?, bool>? canExecute = null)
+        {
+            _execute = execute;
+            _canExecute = canExecute ?? (_ => true);
+        }
+    
+        public bool CanExecute(object? parameter) => _canExecute(parameter);
+    
+        public void Execute(object? parameter)
+        {
+            if (parameter != null) _execute(parameter);
+        }
+    
+    
+        public event EventHandler? CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+    }
+
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private string _selectedImagePath;
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public ICommand LoadFile { get; }
+
+        public string SelectedImagePath
+        {
+            get => _selectedImagePath;
+            set
+            {
+                if (value == _selectedImagePath) return;
+                _selectedImagePath = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainWindowViewModel()
+        {
+            LoadFile = new RelayCommand(_ =>
+            {
+                var dialog = new OpenFileDialog {Filter = "Image Files(*.BMP;*.JPG;*.PNG)|*.BMP;*.JPG;*.PNG"};
+                if (dialog.ShowDialog() == true) SelectedImagePath = dialog.FileName;
+            }, _=>true );
+        }
+    }
+}
