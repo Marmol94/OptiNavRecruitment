a4a4f475d87ac75ef8195247b2b5958d26a8789c
Marmol94 <michmarmol94@gmail.com>
2020-12-04 17:06:29 +0100

    Execution Timer - name change

--- ImageProcessingLibrary/ExecutionTimer.cs
+++ ImageProcessingLibrary/ExecutionTimer.cs
@@ -4,12 +4,12 @@ using System.Threading.Tasks;
 
 namespace ImageProcessingLibrary
 {
-    public class ExecutionTime<T>
+    public class TimedExecution<T>
     {
         private T Result { get; }
         private TimeSpan Duration { get; }
 
-        public ExecutionTime(T result, TimeSpan duration)
+        public TimedExecution(T result, TimeSpan duration)
         {
             Result = result;
             Duration = duration;
@@ -17,15 +17,14 @@ namespace ImageProcessingLibrary
     }
     public static class Timer{
         
-        
-        public static ExecutionTime<T> Measure<T>(Func<T> functionToMeasure) {
+        public static TimedExecution<T> Measure<T>(Func<T> functionToMeasure) {
             var stopwatch = Stopwatch.StartNew();
             var result = functionToMeasure();
             stopwatch.Stop();
-            return new ExecutionTime<T>(result, stopwatch.Elapsed);
+            return new TimedExecution<T>(result, stopwatch.Elapsed);
         }
 
-        public static ExecutionTime<T> MeasureAsync<T>(Task<T> functionToMeasure)
+        public static TimedExecution<T> MeasureAsync<T>(Task<T> functionToMeasure)
         {
             
         }
