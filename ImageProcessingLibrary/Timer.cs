f31b2eb1a4cf633c6729d644c009ba0171c8cc36
Marmol94 <michmarmol94@gmail.com>
2020-12-06 00:57:22 +0100

    Renamed ExecutionTimer to Timer etc

--- ImageProcessingLibrary/Timer.cs
+++ ImageProcessingLibrary/Timer.cs
@@ -4,35 +4,35 @@ using System.Threading.Tasks;
 
 namespace ImageProcessingLibrary
 {
-    public class TimedExecution<T>
+    public class Timed<T>
     {
         private T Result { get; }
         private TimeSpan Duration { get; }
 
-        public TimedExecution(T result, TimeSpan duration)
+        public Timed(T result, TimeSpan duration)
         {
             Result = result;
             Duration = duration;
         }
     }
 
-    public static class MethodTimer
+    public static class Timer
     {
-        public static TimedExecution<T> Measure<T>(Func<T> functionToMeasure)
+        public static Timed<T> Measure<T>(Func<T> functionToMeasure)
         {
             var stopwatch = Stopwatch.StartNew();
             var result = functionToMeasure();
             stopwatch.Stop();
-            return new TimedExecution<T>(result, stopwatch.Elapsed);
+            return new Timed<T>(result, stopwatch.Elapsed);
         }
 
-        public static Task<TimedExecution<T>> MeasureAsync<T>(Task<T> functionToMeasure)
+        public static Task<Timed<T>> MeasureAsync<T>(Task<T> functionToMeasure)
         {
             var stopwatch = Stopwatch.StartNew();
             return functionToMeasure.ContinueWith(task =>
             {
                 stopwatch.Stop();
-                return new TimedExecution<T>(task.Result, stopwatch.Elapsed);
+                return new Timed<T>(task.Result, stopwatch.Elapsed);
             });
         }
     }
